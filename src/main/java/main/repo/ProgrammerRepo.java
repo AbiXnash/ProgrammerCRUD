0d63124de555fd7471a640d7797eeaa6b1ae2abc
Abinash <abixnash@gmail.com>
2025-06-10 16:34:49 +0530

    Crud -> Jpa repo

--- src/main/java/main/repo/ProgrammerRepo.java
+++ src/main/java/main/repo/ProgrammerRepo.java
@@ -2,10 +2,10 @@ package main.repo;
 
 import main.Programmer;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ProgrammerRepo extends CrudRepository<Programmer, Integer> {
+public interface ProgrammerRepo extends JpaRepository<Programmer, Integer> {
     List<Programmer> findByName(String name);
 }
